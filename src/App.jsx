100c4bb8b2c16f3cf86b238cd6e8d82a35102715
bheem-works <shokokomichan098@gmail.com>
2025-06-22 15:26:08 +0545

    quesitoning about the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ArticleOne from './components/ArticleOne'
+import Game from './components/gameArticle'
+
 function App() {
   return (
     <div>
-      <ArticleOne />
+      <Game />
     </div>
   )
 }
---
+++ src/components/gameArticle.jsx
@@ -0,0 +1,24 @@
+import {React} from 'react';
+
+function Game () {
+  return (
+    <>
+    <div className='gameContainer'>
+      <div className="title">
+      <h1>Game in JavaScript</h1>
+      </div>
+      <div className="intro">
+        <p>Is making game, Is Different from making the online website? </p>
+        <p>I think i am really and stupid person who is doing nothings. Just feels laziness. I don't have focus on what i am making. 
+        and i have never make game and the thing's that i have done with my self. i was relly on the toutorial youtubes videos and i just got stuck 
+        and what ever youtubes teach me i just did that. I just need to build something's what'st comes in my mind small thing's but i have to 
+        build that thing's with myself. No other's youtubes toturials ai help. Just me who is going to bild. And i want to be confident about it. 
+        well making the games and doing the all stuff projects. Will that help me to become a good programmer or coder? and can i be able to solve the
+      leetcode problems. Like if it is then i am going to tyr and i will make stuff with my own self. </p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Game;
