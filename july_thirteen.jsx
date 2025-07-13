311b933de6c5df35e74e6848516f4841fa7ddbe9
bheem-works <shokokomichan098@gmail.com>
2025-07-14 00:01:32 +0545

    journaling complete for the todayg

---
+++ july_thirteen.jsx
@@ -0,0 +1,33 @@
+    import React from 'react'
+    
+    function july_thirteen() {
+      return (
+        <div className='container'>
+            <div className="start">
+          <h1>July 13th</h1>
+          <p>This is a special day in July.</p>
+          <p>Let's start from today. Today weather was pretty and it's so sunny. And yeh after all today day was pretty fine. I enjoy it. 
+            <br /> <b>Today was a good day.</b> <strong>When i talk in english i feel confident. and i don't do lot more jhagda(quarriel) 
+            </strong> Which is a good thing. I feel like i doesn't care about stupid things.And i am a <b>Chill person</b>
+            Anways, that's it for today. I will write some stuff on the event class. 
+            <strong>The account books was also not avaible today in the shop.</strong>
+          </p>
+        
+            <div className="event">
+                <h1>Event**</h1>
+                <p>Today event i talk about lot about the big thing's and the improvements thing's and the c stuff and much more stuff. 
+                    talking with a person that really gives up a hope and a knowledge about the thing's what you want to listen to. 
+                    I am quite happy i have a mentor like that who is from the us and other people also who helped a lot. 
+                    And yeh i have to make the 100 code today and i have to do my best in the english homework.. 
+                    <br /> <b>And i have to do the 100 code today.</b>
+                    <i>Jane See ya!</i>
+                </p>
+            </div>
+            </div>
+
+        </div>
+      )
+    }
+    
+    export default july_thirteen
+    
