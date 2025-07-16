6666c04556e189c4047de60ea3cf1ddca3f7ba4f
bheem-works <shokokomichan098@gmail.com>
2025-07-16 23:36:23 +0545

    donot read this...

--- july_sixteen.jsx
+++ july_sixteen.jsx
@@ -15,6 +15,42 @@ function july_sixteen() {
             but i did it. And i am happy about it. And the day was pretty fine
             <h1>And yeh  that's the end of the july 15!</h1>
         </p>
+        <p>
+            <br />
+            <br />
+            <h1>It's about the july 16</h1>
+            So, the today was morning was quite rush but it is fine i have prepared all the thing's before. 
+            And i went to the college and the day is pretty fine. And i am kind of scared with the egnlsih stuff
+            there is no any special event. But yeh there are some even which i have to tak about todya. 
+
+            <br />
+            <br />
+            My personal life. I think mrs.vim is always hiding something frome me and she scared that if i told something's 
+            wrong then i might get in the trouble or his heart will broke. That's it. There is nothing special about it.
+            hiding from me. well i i don'g like hidden seek game in the relaiton ships. And yeh she jsut stareted to hurt
+            But listening by here the thing's cna be clear but listen by some one my hert almost got crack at that time. 
+            And i am not sure about it. But i think she is hiding something from me. And i don't like it.
+            And i have talked to her about it. The four thing's that's we must need to continue and protect the realation ships
+            and the thing's are that are trust,honesty,loyalyty and priority that are four thing's that are essential and the
+            the necessity of the love. And i some times thinnk that she is not doing that. She is avoiding something's like that. 
+            There can be couple of issued in the realtion ships but it is okay. If the two person can solved that issued with
+            by taking the help from the each other then it is fine. But if the one person is not. And i also tell her that 
+            i might take a revenge about it what you done to me and she might be shocked or may be take some wrong idea. 
+            But i kow i have tell that. And i even take a mom promise so any how i have to do that. 
+        </p>
+      </div>
+      <div className="event">
+        Event*
+        <br />
+        So,Today even we got the home part becasue it is the last day of the months and it is quite fun. 
+        And we have to do the home part and it is quite fun. And i am happy about it. And the day was pretty fine.
+        I have not eaten food and i have taken the promis and yeh. that's it. 
+        Life will make you feel sad and so sad it makes you feel like you are living in the hell.
+        But you have to be strong and you have to be happy. And the life is not easy but it is not hard also.
+        Don't know at the future i am going to be that person or not whom i am right now. 
+        It is depend me but if they keep behavig like this then it is hard for me to think about it. 
+        <br /><br />
+        <i>See yaaaa, Janeeeee.....</i>
       </div>
     </div>
   )
