15b9f9040f3ca2d0dd135d00cee62040abf1a36f
bheem-works <shokokomichan098@gmail.com>
2025-07-16 23:19:53 +0545

    new commit

---
+++ july_sixteen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function july_sixteen() {
+  return (
+    <div className='contianer'>
+      <div className="start">
+        <p>
+            <h1>I want to talk a little bit about the july 15 </h1>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default july_sixteen
