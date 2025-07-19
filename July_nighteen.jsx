8237ccae0225d8a9c7b839cb18f095518e221f2c
bheem-works <shokokomichan098@gmail.com>
2025-07-19 23:33:15 +0545

    Jounral of day

---
+++ July_nighteen.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function July_nighteen() {
+  return (
+    <div>
+      <p>I can say today was the pretty good day. I have done so much coding don't know what i learned but i just learend. 
+        don't know what i do but i have just done. 
+        And yeh i just do a little bit change and i think i can bring a change to my self. 
+        and yeh the day was pretty cool 
+      </p>
+      <p>
+        Event *
+        pretty good day wakeup fast and much more stuff and yeh i will do my practise continue and keep learning thing's. Thats it
+        nothing' special and i went to the thupa pasal with my friends. And yeh highness completed my not and i am thankfull for that.
+        <i>see yaa.. janeee</i>
+      </p>
+    </div>
+  )
+}
+
+export default July_nighteen
