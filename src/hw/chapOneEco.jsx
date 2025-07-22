72ca9dcf87d54274d11c7b271254e9853405e1e8
bheem-works <shokokomichan098@gmail.com>
2025-07-22 18:57:33 +0545

    space givven

--- src/hw/chapOneEco.jsx
+++ src/hw/chapOneEco.jsx
@@ -46,22 +46,22 @@ function EconomicsChapOne() {
       question: "Explain the concept allocation of the resources of a central problem of an economy.",
       answer: `Allocation of resources is the central problem faced by all individuals, households and even countries, both rich and poor. This problem arises due to scarcity and choice. 
       <br /><br /> 
-      They are:<br />
-      1. What to produce? <br /> 
+      They are:<br /> 
+      1. What to produce? <br /> <br />  
       -> This is the first problem of the allocation of resources as the economy has to decide what to produce and in what quantity. The economy has to decide and choose whether to produce capital or consumer goods, civilian or defense goods, luxury, comfort or necessity goods etc., and in what quantity of each good. 
-      <br /> 
-      2. How to produce?<br />
+      <br /> <br />  
+      2. How to produce?<br /> <br /> 
       -> This is the second problem of the allocation of resources as the economy has to decide how to produce the goods and services. There are generally two methods of production: labor-intensive and capital-intensive. The economy has to decide which method to use based on the availability of resources and technology.
-      <br />
-      3. For whom to produce?<br />
+      <br /> <br /> 
+      3. For whom to produce?<br /> <br /> 
       -> This is the third problem related to the allocation of resources. The economy has to decide and choose carefully how to distribute the goods and services produced among the rich and the poor so that all benefit and obtain maximum satisfaction.
-      <br />
-      4. Efficient utilization of resources<br />
+      <br /> <br /> 
+      4. Efficient utilization of resources<br /> <br /> 
       -> This is the fourth problem of the allocation of resources where the country has to ensure full employment and best use of resources without any wastages and obtain maximum satisfaction at minimum cost and efforts.
-      <br />
-      5. How to achieve maximum use of resources?<br />
+      <br /> <br /> 
+      5. How to achieve maximum use of resources?<br /> <br /> 
       -> This is the fifth problem of the allocation of resources. Maximum economic growth can be achieved by making the best use of resources with latest technology in order to achieve maximum use of resources. The economy has to allocate its resources and technology in order to achieve maximum economic growth. These problems of allocation of resources are solved depending upon the economic system and type of markets. 
-      <br />
+      <br /><br /> 
       In a capitalist economy, the price mechanism based on demand and supply solves these problems.<br />
       In a socialist economy, the government plays a significant role in resource allocation.<br />
       In a mixed economy, both the price mechanism and government intervention are used to solve these problems.<br />`
